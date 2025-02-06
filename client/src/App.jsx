646dcbdc5949fc45f0e2d4834407094966136b15
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-02-06 15:18:51 -0400

    Completed login route, and added full auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
-import LoginPage from './pages/Login/loginpage.jsx';
-import { ThemeProvider } from './components/dark-mode-provider/theme-provider';
-import { RecoilRoot } from 'recoil';
+// import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+// import LoginPage from './pages/Login/loginpage.jsx';
+// import Dashboard from './pages/Dashboard/Dashboard.jsx'
+// import { ThemeProvider } from './components/dark-mode-provider/theme-provider';
+// import { RecoilRoot } from 'recoil';
+// import PrivateRoute from './components/private-route/private-route.jsx';
 
-function App() {
-  return (
-    <>
-      <RecoilRoot>
-        <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<LoginPage />} />
-            </Routes>
-          </BrowserRouter>
-        </ThemeProvider>
-      </RecoilRoot>
-    </>
-  )
-}
+// function App() {
+//   return (
+//     <>
+//       <RecoilRoot>
+//         <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+//           <BrowserRouter>
+//             <Routes>
+//               <Route path="/" element={<LoginPage />} />
+//               <PrivateRoute path="/home" element={<Dashboard />} />
+//             </Routes>
+//           </BrowserRouter>
+//         </ThemeProvider>
+//       </RecoilRoot>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
