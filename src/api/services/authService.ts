5808bee422439b5ccad545b1c0e26791c8a1b42c
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-03-17 23:11:43 -0300

    Added getMyUser function

--- src/api/services/authService.ts
+++ src/api/services/authService.ts
@@ -1,4 +1,4 @@
-import Users from '../db/userModel.ts';
+import Users from '../db/userModel';
 import jwt from 'jsonwebtoken';
 
 export const signup = async ({ email, password, name }) => {
@@ -11,6 +11,12 @@ export const signup = async ({ email, password, name }) => {
 export const login = async ({ email, password }) => {
   const user = await Users.findOne({ email }).select('+password');
   if (!user || !(await user.matchPassword(password))) throw new Error('Invalid credentials');
-  const token = jwt.sign({ email }, process.env.JWT_SECRET);
+  const token = jwt.sign({ id: user._id.toString(), email }, process.env.JWT_SECRET);
   return { token, user };
 };
+
+export const getMyUser = async (email: String ) => {
+  const user = await Users.findOne({email: email});
+  if(user !== null || user !== undefined) throw new Error("User does not exist");
+  return user;
+}
