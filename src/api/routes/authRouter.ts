7b802cf346030cda0135caabc79a63a948d48f4b
Gurleen <gurleenwadhwa.sk@gmail.com>
2025-03-18 06:01:51 -0300

    added /me endpoint to get user profile info

--- src/api/routes/authRouter.ts
+++ src/api/routes/authRouter.ts
@@ -1,9 +1,12 @@
-import { Router } from 'express';
-import { signupController, loginController } from '../controllers/authController.ts';
+import { Router }from "express";
+import authMiddleware from "../middlewares/authMiddleware";
+
+import { signupController, loginController, getMyProfile } from '../controllers/authController';
 
 const router = Router();
 
 router.post('/signup', signupController);
 router.post('/login', loginController);
+router.post('/me', authMiddleware, getMyProfile);
 
 export default router;
